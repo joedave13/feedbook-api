9165f6c1c4c8449855e2de07beaa86a4ab4af576
Joshua Davian <joshuadavian@gmail.com>
2022-11-06 19:24:25 +0700

    migration model & seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+            ->has(Post::factory()->count(3))
+            ->count(5)
+            ->create();
+    }
+}
